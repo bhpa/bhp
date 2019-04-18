c885d9b0807175f487f8c1a912e2bc8fba8a5273
SkyeBlack <296471989@qq.com>
2019-04-18 17:10:57 +0800

    add ConsensusMessageType Commit and RecoveryMessage

---
+++ bhp/Consensus/RecoveryMessage.cs
@@ -0,0 +1,17 @@
+﻿using Bhp.IO;
+using Bhp.Ledger;
+using Bhp.Network.P2P.Payloads;
+using Bhp.SmartContract;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Bhp.Consensus
+{
+    public partial class RecoveryMessage : ConsensusMessage
+    {
+        public RecoveryMessage() : base(ConsensusMessageType.RecoveryMessage)
+        {
+        }
+    }
+}
