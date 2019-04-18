30c5c19554374ee2c9d8b868c4ce3883bdfefa03
SkyeBlack <296471989@qq.com>
2019-04-18 15:46:46 +0800

    add ConsensusMessageType Commit and RecoveryMessage

--- bhp/Consensus/ConsensusMessageType.cs
+++ bhp/Consensus/ConsensusMessageType.cs
@@ -2,13 +2,19 @@
 
 namespace Bhp.Consensus
 {
-    internal enum ConsensusMessageType : byte
+    public enum ConsensusMessageType : byte
     {
         [ReflectionCache(typeof(ChangeView))]
         ChangeView = 0x00,
+
         [ReflectionCache(typeof(PrepareRequest))]
         PrepareRequest = 0x20,
         [ReflectionCache(typeof(PrepareResponse))]
         PrepareResponse = 0x21,
+        [ReflectionCache(typeof(Commit))]
+        Commit = 0x30,
+
+        [ReflectionCache(typeof(RecoveryMessage))]
+        RecoveryMessage = 0x41,
     }
 }
