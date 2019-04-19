d9169435562b28bba98517f75604bc06d7b67c1a
SkyeBlack <296471989@qq.com>
2019-04-19 15:38:19 +0800

    Fix memory leak as transactions are continuously received.

--- bhp/Network/P2P/ProtocolHandler.cs
+++ bhp/Network/P2P/ProtocolHandler.cs
@@ -20,8 +20,8 @@ namespace Bhp.Network.P2P
         public class SetFilter { public BloomFilter Filter; }
 
         private readonly BhpSystem system;
-        private readonly HashSet<UInt256> knownHashes = new HashSet<UInt256>();
-        private readonly HashSet<UInt256> sentHashes = new HashSet<UInt256>();
+        private readonly FIFOSet<UInt256> knownHashes;
+        private readonly FIFOSet<UInt256> sentHashes;        
         private VersionPayload version;
         private bool verack = false;
         private BloomFilter bloom_filter;
@@ -29,6 +29,8 @@ namespace Bhp.Network.P2P
         public ProtocolHandler(BhpSystem system)
         {
             this.system = system;
+            this.knownHashes = new FIFOSet<UInt256>(Blockchain.Singleton.MemPool.Capacity * 2);
+            this.sentHashes = new FIFOSet<UInt256>(Blockchain.Singleton.MemPool.Capacity * 2);
         }
 
         protected override void OnReceive(object message)
