976b24979b25b97cdf60f0dfd275050104c96a5d
Reskorey <1204490370@qq.com>
2019-08-26 10:13:09 +0800

    Update VM to last changes

---
+++ bhp/SmartContract/ExecutionContextState.cs
@@ -0,0 +1,10 @@
+﻿namespace Bhp.SmartContract
+{
+    public class ExecutionContextState
+    {
+        /// <summary>
+        /// Script hash
+        /// </summary>
+        public UInt160 ScriptHash { get; set; }
+    }
+}
